a1a8e628a6d05fb9f34b901301f9bdf8ba8ccf00
Wendim <wendim878@gmail.com>
2024-04-29 14:18:24 +0300

    add a new route to

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send("Hello this is home page")
+})
 app.get("/names", (req, res) => {
     res.json({
         name: "Wendim"
